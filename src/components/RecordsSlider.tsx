25958b200da7f62b2ca028e31707c96846b2bbc5
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-08-19 17:38:17 -0300

    [UPDATE]: Update RecordsSlider

--- src/components/RecordsSlider.tsx
+++ src/components/RecordsSlider.tsx
@@ -1,5 +1,6 @@
 import { RegisterWithId } from '@/services/records'
 import RecordCard from './RecordCard'
+import { decrypt } from '@/lib/encryption'
 
 interface Props {
 	records: RegisterWithId[]
@@ -9,9 +10,18 @@ export default function RecordsSlider({ records }: Props) {
 	return (
 		<div className='py-2'>
 			<section className='slides'>
-				{records?.map((record) => (
-					<RecordCard record={record} key={record.email} />
-				))}
+				{records.map((record) => {
+					const parsedRecord: RegisterWithId = {
+						color: record.color,
+						keys: record.keys,
+						email: decrypt(record.email),
+						password: decrypt(record.password),
+						site: decrypt(record.site),
+						user: decrypt(record.user),
+					}
+
+					return <RecordCard record={parsedRecord} key={record.email} />
+				})}
 			</section>
 		</div>
 	)
