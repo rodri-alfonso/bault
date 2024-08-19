e95f3630f86a165b2ca0ec3c113fc3422605c898
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-08-19 17:38:29 -0300

    [UPDATE]: Update ColorPicker component

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from 'react'
 import { CARDS_COLORS } from '@/lib/colors'
 import { TwitterPicker } from 'react-color'
+import { PaintBucketIcon } from '@/assets/icons'
 
 interface Props {
 	color: string
@@ -20,10 +21,14 @@ export default function ColorPicker({ color, setColor }: Props) {
 		<div className='flex flex-col justify-end items-end gap-2'>
 			<button
 				onClick={() => setIsModalOpen(!isModalOpen)}
-				className='rounded-xl p-3 bg-gray-50 grid place-items-center active:scale-95 transition-all hover:bg-gray-100'
+				className='grid place-items-center p-2 bg-gray-50 hover:bg-gray-100 text-gray-800 rounded-xl active:scale-95 transition-all'
 				ref={pickerRef}
+				style={{
+					backgroundColor: color + '4D',
+				}}
 			>
-				<div className='bg-blue-800 rounded-full w-4 h-4' style={{ backgroundColor: color || color || 'whitesmoke' }} />
+				{/* <div className='bg-blue-800 rounded-full w-4 h-4' style={{ backgroundColor: color || color || 'whitesmoke' }} /> */}
+				<PaintBucketIcon />
 			</button>
 
 			{isModalOpen && (
