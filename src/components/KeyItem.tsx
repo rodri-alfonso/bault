48371ddd209ef01469bebe4b98ab5c97d8615228
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-08-21 18:40:03 -0300

    [FEATURE]: Create KeyItem component

---
+++ src/components/KeyItem.tsx
@@ -0,0 +1,38 @@
+import { CopyIcon, TickIcon } from "@/assets/icons"
+
+interface Props {
+    checked: boolean
+    onCheck: () => void
+}
+
+export default function KeyItem({checked, onCheck}: Props){
+    return(
+        <div
+        className={`h-10 flex items-center text-gray-400 gap-3 w-full ${
+            checked ? 'justify-start' : ''
+        }`}
+    >
+        <button
+            onClick={onCheck}
+            className={`active:scale-95 transition-all w-6 overflow-hidden h-6 border-gray-700 rounded-lg border-solid border-2 grid place-items-center
+            ${checked ? 'bg-gray-700' : 'bg-white'}
+            `}
+        >
+            {checked && <TickIcon className={`w-full h-full text-white bg-gray-700`} />}
+        </button>
+        <p
+            className={`truncate w-auto max-w-[200px] ${
+                checked ? 'line-through text-gray-300 font-medium' : 'font-medium text-gray-500'
+            }`}
+        >
+            aaksljdhaskjdhzasdsadasdasddasdkljhasdasdasd
+        </p>
+
+        <button
+            className='ml-auto text-white rounded-full p-2 active:scale-95 transition-all z-20 bg-gray-800'
+        >
+            <CopyIcon className='w-5 h-5' />
+        </button>
+    </div>
+    )
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,14 @@
+export type Key = {
+	value: string
+	checked: boolean
+}
+
 export interface Register {
 	email: string
 	user: string
 	password: string
 	site: string
-	keys: string[]
+	keys: Key[]
 	color: string
 	marked: boolean
 	id?: string
