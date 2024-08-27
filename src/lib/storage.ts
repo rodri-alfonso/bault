f249def0cedde59c0d98b9d410b71d18be4d8ec2
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-08-26 23:33:08 -0300

    [UPDATE]: Update storage file

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -3,10 +3,20 @@ import type { User } from '../stores/types'
 
 const { LOCAL_STORAGE_KEY } = ENVIRONMENTS
 
+// STORAGE AUTH
 export const getAuthStorage = (): User | null => {
-	const user = localStorage.getItem(LOCAL_STORAGE_KEY)
-	return user ? JSON.parse(user) : null
+  const user = localStorage.getItem(LOCAL_STORAGE_KEY)
+  return user ? JSON.parse(user) : null
 }
-
 export const setAuthStorage = (user: User) => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(user))
 export const removeAuthStorage = () => localStorage.removeItem(LOCAL_STORAGE_KEY)
+
+// STORAGE EXPIRATION
+
+export const getExpirationStorage = (): number => {
+  const expiration = JSON.parse(localStorage.getItem('expiration') ?? '')
+  return expiration || 0
+}
+export const setExpirationStorage = (expiration: number) =>
+  localStorage.setItem('expiration', JSON.stringify(expiration))
+export const removeExpirationStorage = () => localStorage.removeItem('expiration')
