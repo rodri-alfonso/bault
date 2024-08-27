ec0006d135bc6864ebb9c0664229dbc0f1b7b5d0
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-08-26 23:33:52 -0300

    [UPDATE]: Update Home and App pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,46 @@ import { Route, Switch, Redirect } from 'wouter'
 import LoginPage from './pages/Login'
 import { authStore } from './stores/auth'
 import RecordPage from './pages/Record'
+import { timestampStore } from './stores/timestamp'
+import SecurityPage from './pages/Security'
+import { registerStore } from './stores/security'
+import RegisterPage from './pages/Register'
+import useAuthState from './hooks/useAuthState'
+import LoaderPage from './pages/Loader'
 
 function PrivateRouter() {
-	return (
-		<Switch>
-			<Route path='/' component={HomePage} />
-			<Route path='/create' component={CreatorPage} />
-			<Route path='/record/:id' component={RecordPage} />
-			<Redirect to='/' />
-		</Switch>
-	)
+  return (
+    <Switch>
+      <Route path='/' component={HomePage} />
+      <Route path='/create' component={CreatorPage} />
+      <Route path='/record/:id' component={RecordPage} />
+      <Redirect to='/' />
+    </Switch>
+  )
 }
 
 function App() {
-	const { user } = authStore()
+  const { user, setUser } = authStore()
+  const { isProtected } = timestampStore()
+  const { isInRegisterCode } = registerStore()
 
-	if (user) return <PrivateRouter />
+  const { loading } = useAuthState()
 
-	return (
-		<Switch>
-			<Route path='/' component={LoginPage} />
-			<Redirect to='/' />
-		</Switch>
-	)
+  // const isTimestampPassedAfterTwoMinutes = new Date().getTime() - timestamp > 120000
+  // console.log('🚀 ~ App ~ isTimestampPassedAfterTwoMinutes:', isTimestampPassedAfterTwoMinutes)
+
+  if (loading) return <LoaderPage />
+
+  if (isInRegisterCode) return <RegisterPage />
+  // if (isProtected) return <SecurityPage />
+  if (user) return <PrivateRouter />
+
+  return (
+    <Switch>
+      <Route path='/' component={LoginPage} />
+      <Redirect to='/' />
+    </Switch>
+  )
 }
 
 export default App
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,71 +6,69 @@ import { authStore } from '@/stores/auth'
 import { decrypt } from '@/lib/encryption'
 import { ArrowRightIcon, PlusIcon } from '@/assets/icons'
 import Record from '@/components/Reacord'
-import Navbar from '@/components/Navbar'
 import RecordsSlider from '@/components/RecordsSlider'
 import { useLocation } from 'wouter'
 
 export default function HomePage() {
-	const { user } = authStore()
-	const { data, isLoading } = useFetch(getRecords)
-	const [_, navigate] = useLocation()
+  const { user } = authStore()
+  const { data, isLoading } = useFetch(getRecords)
+  const [_, navigate] = useLocation()
 
-	const firstName = decrypt(user?.name ?? '').split(' ')[0]
+  const firstName = decrypt(user?.name ?? '').split(' ')[0]
 
-	const isLoadingData = !data?.length && isLoading
+  const isLoadingData = !data?.length && isLoading
 
-	return (
-		<Page className='flex flex-col gap-4'>
-			<Header />
-			<section className='pt-2'>
-				<p className='text-2xl font-medium'>{`Hello ${firstName},`}</p>
-				<p className='text-gray-500 font-medium'>Let's check your bault!</p>
-			</section>
+  return (
+    <Page className='flex flex-col gap-4'>
+      <Header />
+      <section className='pt-2'>
+        <p className='text-2xl font-medium'>{`Hello ${firstName},`}</p>
+        <p className='text-gray-500 font-medium'>Let's check your bault!</p>
+      </section>
+      {isLoadingData ? (
+        <div className='w-full bg-gray-200 rounded-3xl py-12  mb-auto' />
+      ) : (
+        <RecordsSlider records={data?.filter((record) => record.marked) || []} />
+      )}
 
-			{isLoadingData ? (
-				<div className='w-full bg-gray-200 rounded-3xl py-12  mb-auto' />
-			) : (
-				<RecordsSlider records={data?.filter((record) => record.marked) || []} />
-			)}
+      <div className='flex items-center justify-between'>
+        <p className='text-lg font-medium'>Last records</p>
+        {isLoadingData ? (
+          <div />
+        ) : (
+          <button className='p-2 rounded-lg hover:bg-gray-100 transition-all active:scale-95'>
+            <ArrowRightIcon />
+          </button>
+        )}
+      </div>
 
-			<div className='flex items-center justify-between'>
-				<p className='text-lg font-medium'>Last records</p>
-				{isLoadingData ? (
-					<div />
-				) : (
-					<button className='p-2 rounded-lg hover:bg-gray-100 transition-all active:scale-95'>
-						<ArrowRightIcon />
-					</button>
-				)}
-			</div>
+      <section className='grid gap-2 overflow-y-auto mb-auto'>
+        {isLoadingData ? (
+          <>
+            <div className='w-full bg-gray-200 rounded-xl py-8' />
+            <div className='w-full bg-gray-200 rounded-xl py-8' />
+            <div className='w-full bg-gray-200 rounded-xl py-8' />
+            <div className='w-full bg-gray-200 rounded-xl py-8' />
+          </>
+        ) : (
+          <>
+            <button
+              className={`border-2 border-solid border-gray-200 rounded-2xl h-16 p-1 pr-3 flex items-center gap-4 transition-all hover:bg-gray-50`}
+              onClick={() => navigate('/create')}
+            >
+              <div className='grid place-items-center h-full w-12 rounded-2xl text-gray-200 text-lg font-semibold  bg-gray-100'>
+                <PlusIcon className='w-6 h-6 text-gray-400' />
+              </div>
+              <p className=' text-gray-400'>Create new record</p>
+            </button>
+            {data?.map((record) => (
+              <Record {...record} key={record.id} />
+            ))}
+          </>
+        )}
+      </section>
 
-			<section className='grid gap-2 overflow-y-auto mb-auto'>
-				{isLoadingData ? (
-					<>
-						<div className='w-full bg-gray-200 rounded-xl py-8' />
-						<div className='w-full bg-gray-200 rounded-xl py-8' />
-						<div className='w-full bg-gray-200 rounded-xl py-8' />
-						<div className='w-full bg-gray-200 rounded-xl py-8' />
-					</>
-				) : (
-					<>
-						<button
-							className={`border-2 border-solid border-gray-200 rounded-2xl h-16 p-1 pr-3 flex items-center gap-4 transition-all hover:bg-gray-50`}
-							onClick={() => navigate('/create')}
-						>
-							<div className='grid place-items-center h-full w-12 rounded-2xl text-gray-200 text-lg font-semibold  bg-gray-100'>
-								<PlusIcon className='w-6 h-6 text-gray-400' />
-							</div>
-							<p className=' text-gray-400'>Create new record</p>
-						</button>
-						{data?.map((record) => (
-							<Record {...record} key={record.id} />
-						))}
-					</>
-				)}
-			</section>
-
-			{/* <Navbar /> */}
-		</Page>
-	)
+      {/* <Navbar /> */}
+    </Page>
+  )
 }
