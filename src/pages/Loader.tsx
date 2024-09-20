5bc53724920115c21fe4d4d74f827bf4cc2ed26c
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-09-20 16:44:23 -0300

    [FEATURE]: Create Loader page

--- src/pages/Loader.tsx
+++ src/pages/Loader.tsx
@@ -1,3 +1,13 @@
+import { Logo } from '@/assets/logo'
+
 export default function LoaderPage() {
-  return <div>Spinner screen!</div>
+  return (
+    <div className='grid place-items-center h-screen w-full gap-4'>
+      <div className='grid gap-1 place-items-center text-gray-800 animate-pulse'>
+        <Logo className='w-16 h-16' />
+        <p className='text-xl font-extra-black text-center mb-10 uppercase'>Bault</p>
+        {/* <div className='loader'></div> */}
+      </div>
+    </div>
+  )
 }
