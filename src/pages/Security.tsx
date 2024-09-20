39e1377ce945165313437b289334e87e4fa7b0d9
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-09-20 16:44:42 -0300

    [FEATURE]: Create Security page

--- src/pages/Security.tsx
+++ src/pages/Security.tsx
@@ -2,19 +2,43 @@ import { useState } from 'react'
 import Button from '@/theme/Button'
 import OtpInput, { AllowedInputTypes } from 'react-otp-input'
 import { timestampStore } from '@/stores/timestamp'
+import { useLocation } from 'wouter'
+import { ViewIcon, ViewOffIcon } from '@/assets/icons'
+import { isSecureMatch } from '@/services/security'
+import { authStore } from '@/stores/auth'
+import { decrypt, encrypt } from '@/lib/encryption'
 
 const MAX_NUMBER_INPUTS = 4
 
 export default function SecurityPage() {
+  const { user } = authStore()
   const [otp, setOtp] = useState('')
   const [type, setType] = useState<AllowedInputTypes>('password')
-  const { setIsProtected } = timestampStore()
+  const { setIsProtected, setTimestamp } = timestampStore()
+  const [_, navigate] = useLocation()
+  const [isLoading, setIsLoading] = useState(false)
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault()
     if (!otp.length) return
 
-    setIsProtected(false)
+    setIsLoading(true)
+
+    isSecureMatch(user?.id || '', otp)
+      .then((isVerificated) => {
+        if (!isVerificated) {
+          alert('Invalid code')
+          setOtp('')
+          return
+        }
+
+        setTimestamp()
+        setIsProtected(false)
+        navigate('/')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
   }
 
   function handleChangeVisibility() {
@@ -22,26 +46,39 @@ export default function SecurityPage() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>I'm the Security page</h1>
-
+    <form onSubmit={handleSubmit} className='h-screen grid'>
+      <div>
+        <h1 className='text-2xl font-bold text-center mt-8'>Verification</h1>
+        <p className='text-center text-gray-500'>Enter the 4-digit code sent to your email</p>
+      </div>
       <OtpInput
         value={otp}
         onChange={setOtp}
         numInputs={MAX_NUMBER_INPUTS}
         renderSeparator={<span>-</span>}
         renderInput={(props) => <input {...props} />}
-        containerStyle={'p-8 w-full'}
+        containerStyle={'p-8 w-full m-auto'}
         inputType={type}
-        inputStyle={'border-2 border-black w-[50px] h-[50px] p-2 flex justify-center items-center'}
+        inputStyle={
+          'w-[80px] text-xl font-medium h-[80px] p-2 rounded-xl bg-gray-200 text-center flex justify-center items-center m-auto'
+        }
         skipDefaultStyles
         shouldAutoFocus
       />
+      <div className='m-auto flex gap-2'>
+        <button type='button' onClick={handleChangeVisibility}>
+          Change visibility
+        </button>
 
-      <button type='button' onClick={handleChangeVisibility}>
-        Change visibility
-      </button>
-      <Button label='Send' disabled={otp.length !== MAX_NUMBER_INPUTS} loading={false} />
+        {type === 'password' ? <ViewIcon /> : <ViewOffIcon />}
+      </div>
+      <div className='grid px-3 mt-auto pb-4'>
+        <Button
+          label={isLoading ? 'Sending...' : 'Send'}
+          disabled={isLoading || otp.length !== MAX_NUMBER_INPUTS}
+          loading={isLoading}
+        />
+      </div>
     </form>
   )
 }
