7714cdabdc0da35502bc460e8c0f39e664546bb0
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-09-26 08:38:54 -0300

    [UPDATE]: Update Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,30 @@
 import { signIn } from '../stores/auth'
+import { Logo2 } from '@/assets/logo'
+import Button from '@/theme/Button'
+import { GoogleIcon } from '@/assets/icons'
 
 export default function LoginPage() {
-	return (
-		<div>
-			<h1>I'm the LoginPage</h1>
-			<button onClick={signIn} className='bg-blue-200 p-2 rounded-md'>
-				Login
-			</button>
-		</div>
-	)
+  return (
+    <div className='grid h-screen mx-auto  sm:max-w-sm'>
+      <div className='w-full h-64 bg-gray-800 rounded-b-[90px] relative grid place-items-center'>
+        <div className='absolute -bottom-14 bg-white rounded-[30px] p-3 m-auto'>
+          <Logo2 className='w-24 h-24' />
+        </div>
+      </div>
+
+      <div className=''>
+        <p className='text-3xl font-extrabold text-center text-gray-800'>Welcome to Bault,</p>
+        <p className='text-lg font-semibold text-gray-600 text-center'>Your new password keeper.</p>
+
+        <div className='grid place-items-center text-center m-auto pt-8 px-10'>
+          <p className='text-base font-medium text-gray-500'>All your passwords saved and secure in one place.</p>
+        </div>
+      </div>
+
+      <div className='mt-auto pb-10 grid gap-4 px-10'>
+        <Button label='Login with Google' onClick={signIn} className='' icon={<GoogleIcon />} />
+        <p className='text-sm text-center font-medium underline'> Or check the playground</p>
+      </div>
+    </div>
+  )
 }
