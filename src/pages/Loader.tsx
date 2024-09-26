7f8c684b3d423ac936df9f5918de43e83a8970e8
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-09-26 08:38:27 -0300

    [UPDATE]: Update Loader

--- src/pages/Loader.tsx
+++ src/pages/Loader.tsx
@@ -1,12 +1,12 @@
-import { Logo } from '@/assets/logo'
+import { Logo2 } from '@/assets/logo'
 
 export default function LoaderPage() {
   return (
     <div className='grid place-items-center h-screen w-full gap-4'>
-      <div className='grid gap-1 place-items-center text-gray-800 animate-pulse'>
-        <Logo className='w-16 h-16' />
-        <p className='text-xl font-extra-black text-center mb-10 uppercase'>Bault</p>
-        {/* <div className='loader'></div> */}
+      <div className='grid gap-4 place-items-center text-gray-800 animate-pulse'>
+        <Logo2 className='w-20 h-20' />
+        {/* <p className='text-xl font-bold text-center mb-10'>Bault</p> */}
+        <div className='loader'></div>
       </div>
     </div>
   )
