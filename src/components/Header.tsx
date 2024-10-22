0e92bd55c7553bf8489182074f8a8acb78547631
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-10-22 18:39:40 -0300

    [UPDATE]: Create ConfirmModal and update Header confirms

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,11 @@
 import { signOut } from '../stores/auth'
 import { authStore } from '../stores/auth'
-// import { decrypt } from '@/lib/encryption'
 import { MenuSquareIcon } from '@/assets/icons'
 import { useLocation } from 'wouter'
 import { ArrowRightIcon, DeleteIcon } from '@/assets/icons'
-import Modal from '@/theme/Modal'
 import { useState } from 'react'
 import { deleteRecord } from '@/services/records'
-import Button from '@/theme/Button'
+import ConfirmModal from './Modals/Confirm'
 
 interface IconButtonProps {
   children: React.ReactNode | React.ReactNode[]
@@ -76,53 +74,27 @@ export default function Header() {
         </section>
       )}
 
-      <Modal isVisible={isModalVisible}>
-        <div className='flex flex-col gap-4 rounded-xl bg-white p-6 m-2'>
-          <div className='text-center'>
-            <h1 className='text-xl font-semibold'>Logout</h1>
-            <p>Are you sure you want to log out?</p>
-          </div>
+      <ConfirmModal
+        isVisible={isModalVisible}
+        onCancel={() => setIsModalVisible(false)}
+        title='Log out of your account?'
+        message='You will need to log back in to access your bault.'
+        confirmLabel='Log out'
+        cancelLabel='Cancel'
+        onConfirm={handleLogout}
+      />
 
-          <div className='grid gap-2'>
-            <button
-              onClick={handleLogout}
-              className='text-sm text-white font-medium bg-gray-800 rounded-lg  p-1.5 active:scale-95 transition-all'
-            >
-              Logout
-            </button>
-            <button
-              onClick={() => setIsModalVisible(false)}
-              className='text-sm font-medium text-gray-800 p-1.5 active:scale-95 rounded-lg transition-all border border-solid border-gray-400 hover:border-gray-800'
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </Modal>
-      <Modal isVisible={isDeleteModalVisible}>
-        <div className='flex flex-col gap-4 rounded-xl bg-white p-6 m-2'>
-          <div className='text-center'>
-            <h1 className='text-xl font-semibold'>Delete</h1>
-            <p>Are you sure you want to delete this record?</p>
-          </div>
-
-          <div className='grid gap-2'>
-            <Button
-              onClick={handleDelete}
-              disabled={isDeleting}
-              label={isDeleting ? 'Deleting...' : 'Delete'}
-              loading={isDeleting}
-            />
-
-            <button
-              onClick={() => setIsDeleteModalVisible(false)}
-              className='text-sm font-medium text-gray-800 p-1.5 active:scale-95 rounded-lg transition-all border border-solid border-gray-400 hover:border-gray-800'
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </Modal>
+      <ConfirmModal
+        isVisible={isDeleteModalVisible}
+        onCancel={() => setIsDeleteModalVisible(false)}
+        title='Delete record'
+        message='Are you sure you want to delete this record?'
+        cancelLabel='Cancel'
+        onConfirm={handleDelete}
+        confirmDisabled={isDeleting}
+        confirmLabel={isDeleting ? 'Deleting...' : 'Delete'}
+        loading={isDeleting}
+      />
     </header>
   )
 }
---
+++ src/components/Modals/Confirm.tsx
@@ -0,0 +1,53 @@
+import Modal from '@/theme/Modal'
+import Button from '@/theme/Button'
+
+interface Props {
+  isVisible: boolean
+  onCancel: () => void
+  onConfirm: () => void
+  title?: string
+  message?: string
+  confirmLabel?: string
+  cancelLabel?: string
+  confirmDisabled?: boolean
+  loading?: boolean
+}
+
+export default function ConfirmModal({
+  isVisible,
+  onConfirm,
+  onCancel,
+  cancelLabel = '',
+  confirmLabel = '',
+  message = '',
+  title = '',
+  confirmDisabled,
+  loading,
+}: Props) {
+  return (
+    <Modal isVisible={isVisible}>
+      <div className='flex flex-col gap-6 rounded-xl bg-white p-6 m-2'>
+        <div className='text-center grid gap-2'>
+          <h1 className='text-lg font-semibold text-gray-800'>{title}</h1>
+          <p className='text-gray-600'>{message}</p>
+        </div>
+
+        <div className='grid gap-2'>
+          <Button
+            onClick={onConfirm}
+            disabled={confirmDisabled}
+            loading={loading}
+            label={confirmLabel}
+            className='bg-red-50 text-red-400 font-medium border border-solid border-red-400 hover:bg-red-400 hover:text-white disabled:bg-red-400 disabled:text-white'
+          />
+
+          <Button
+            onClick={onCancel}
+            label={cancelLabel}
+            className='!bg-gray-50 !text-gray-600 font-medium hover:!bg-gray-800 hover:!text-white border border-solid border-gray-300'
+          />
+        </div>
+      </div>
+    </Modal>
+  )
+}
