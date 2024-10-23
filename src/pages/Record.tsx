6768c67bf9bdafef4c3071d6b3d950ae12846fc0
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-10-23 16:47:53 -0300

    [UPDATE]: Update Record page and sw

--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -4,15 +4,15 @@ import { editRecord, getRecordById } from '@/services/records'
 import { decrypt, encrypt } from '@/lib/encryption'
 import Page from '@/layout/Page'
 import Header from '@/components/Header'
-import { BookmarkIcon, BookmarkFilledIcon, EarthIcon, MailIcon, PasswordIcon, UserIcon } from '@/assets/icons'
+import { EarthIcon, MailIcon, PasswordIcon, UserIcon } from '@/assets/icons'
 import { useEffect, useState } from 'react'
 import RecordCard from '@/components/RecordCard'
 import Input from '@/theme/Input'
 import ColorPicker from '@/components/ColorPicker'
-import SpinnerScreen from '@/components/SpinnerScreen'
 import Button from '@/theme/Button'
 import { Key } from '@/types'
 import KeyItem from '@/components/KeyItem'
+import LoaderPage from './Loader'
 
 export default function RecordPage() {
   const [_, navigate] = useLocation()
@@ -26,7 +26,6 @@ export default function RecordPage() {
   const [user, setUser] = useState('')
   const [color, setColor] = useState('')
   const [keys, setKeys] = useState<Key[]>([])
-  const [isMarked, setIsMarked] = useState(false)
   const [isEnabledButton, setIsEnabledButton] = useState(false)
 
   useEffect(() => {
@@ -37,7 +36,6 @@ export default function RecordPage() {
       setUser(decrypt(data.user))
       setColor(data.color)
       setKeys(data.keys.map((key) => ({ checked: key.checked, value: decrypt(key.value) })))
-      setIsMarked(Boolean(data.marked))
     }
   }, [isLoading])
 
@@ -57,7 +55,6 @@ export default function RecordPage() {
       email: encrypt(email),
       password: encrypt(password),
       user: encrypt(user),
-      marked: isMarked,
       keys: keys.map((key) => ({ ...key, value: encrypt(key.value) })),
       color,
     }
@@ -67,10 +64,7 @@ export default function RecordPage() {
     })
   }
 
-  function handleChange(
-    type: 'site' | 'email' | 'password' | 'user' | 'marked' | 'color' | 'keys',
-    payload: string | boolean
-  ) {
+  function handleChange(type: 'site' | 'email' | 'password' | 'user' | 'color' | 'keys', payload: string | boolean) {
     if (!data) return
 
     const TYPE_MAP = {
@@ -78,7 +72,6 @@ export default function RecordPage() {
       email: setEmail,
       password: setPassword,
       user: setUser,
-      marked: setIsMarked,
       color: setColor,
       keys: toggleKey,
     }
@@ -92,12 +85,11 @@ export default function RecordPage() {
         email === decrypt(data.email) ||
         password === decrypt(data.password) ||
         user === decrypt(data.user) ||
-        isMarked === data.marked ||
         color === data.color
     )
   }
 
-  if (isLoading || !data) return <SpinnerScreen />
+  if (isLoading || !data) return <LoaderPage />
 
   return (
     <Page className='flex flex-col gap-4 relative'>
@@ -106,24 +98,13 @@ export default function RecordPage() {
       <section className='absolute right-16 flex items-center top-6'>
         <ColorPicker color={color} setColor={(payload) => handleChange('color', payload)} />
       </section>
-      <button
-        onClick={() => handleChange('marked', !isMarked)}
-        className='absolute right-28 top-6 p-2 bg-gray-50 hover:bg-gray-100 rounded-xl active:scale-95 transition-all'
-      >
-        {isMarked ? (
-          <BookmarkFilledIcon className='w-6 h-6 text-gray-800' />
-        ) : (
-          <BookmarkIcon className='w-6 h-6 text-gray-500' />
-        )}
-      </button>
 
       <section className='pt-2'>
         <p className='text-2xl font-medium'>Welcome back,</p>
         <p className='text-gray-500 font-medium'>Let's check your record!</p>
       </section>
 
-      {/* <RecordSimpleCard site={site} user={user} color={color} password={data?.password} /> */}
-      <RecordCard record={{ email, site, user, color, password, keys, id, marked: isMarked }} />
+      <RecordCard record={{ email, site, user, color, password, keys, id, marked: data.marked }} />
 
       <p className='font-semibold pt-6'>Information</p>
       <div className='grid gap-3 pb-6'>
