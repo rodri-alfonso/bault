69076bb22f9e6427697cad2df4107d5e1562ac7f
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-11-26 02:08:43 -0300

    [UPDATE]: Update new Layout

--- src/layout/NewPage.tsx
+++ src/layout/NewPage.tsx
@@ -1,4 +1,13 @@
-import { MenuSquareIcon, PasswordIcon, Bookmark01Icon, FloppyDiskIcon, ArrowRightIcon } from '@/assets/icons'
+import {
+  MenuSquareIcon,
+  PasswordIcon,
+  Bookmark01Icon,
+  FloppyDiskIcon,
+  ArrowRightIcon,
+  PlusIcon,
+  KeyIcon,
+  TickIcon,
+} from '@/assets/icons'
 import { Logo2 } from '@/assets/logo'
 import { authStore, signOut } from '@/stores/auth'
 import FavouritesModal, { BookmarkedRecord } from '@/components/Modals/Favourites'
@@ -6,6 +15,7 @@ import { useState } from 'react'
 import ConfirmModal from '@/components/Modals/Confirm'
 import { useRecord } from '@/hooks/useRecords'
 import { useLocation } from 'wouter'
+import GeneratorModal from '@/components/Modals/Generator'
 
 interface Props {
   children: React.ReactNode
@@ -13,16 +23,29 @@ interface Props {
   loadingEditing?: boolean
   disabledEditing?: boolean
   onSaveEditing?: () => void
+  loadingCreating?: boolean
+  disabledCreating?: boolean
+  onSaveCreating?: () => void
 }
 
-export default function NewPage({ children, disabledEditing, loadingEditing, onSaveEditing }: Props) {
+export default function NewPage({
+  children,
+  disabledEditing,
+  loadingEditing,
+  onSaveEditing,
+  disabledCreating,
+  loadingCreating,
+  onSaveCreating,
+}: Props) {
   const { user } = authStore()
   const [isFavouritesModalOpen, setIsFavouritesModalOpen] = useState(false)
   const [isModalVisible, setIsModalVisible] = useState(false)
+  const [isGeneratorVisible, setIsGeneratorVisible] = useState(false)
   const { records, refetch } = useRecord()
   const [location, navigation] = useLocation()
 
   const isRecordPath = location.includes('record/')
+  const isCreatorPath = location === '/create'
 
   function handleLogout() {
     signOut()
@@ -37,7 +60,7 @@ export default function NewPage({ children, disabledEditing, loadingEditing, onS
             onClick={() => navigation('/')}
             className=' text-gray-800 bg-white p-1.5 rounded-xl grid place-items-center active:scale-95 transition-all'
           >
-            {isRecordPath ? <ArrowRightIcon className='w-7 h-7 rotate-180' /> : <Logo2 className='w-7 h-7' />}
+            <Logo2 className='w-7 h-7' />
           </button>
           <div className='grid gap-3'>
             <button
@@ -46,8 +69,11 @@ export default function NewPage({ children, disabledEditing, loadingEditing, onS
             >
               <MenuSquareIcon />
             </button>
-            <button className='text-gray-300 p-2 bg-gray-700 hover:text-white rounded-xl hover:bg-gray-600 active:scale-95 transition-all'>
-              <PasswordIcon />
+            <button
+              onClick={() => setIsGeneratorVisible(true)}
+              className='text-gray-300 p-2 bg-gray-700 hover:text-white rounded-xl hover:bg-gray-600 active:scale-95 transition-all'
+            >
+              <KeyIcon />
             </button>
 
             {isRecordPath ? (
@@ -87,6 +113,44 @@ export default function NewPage({ children, disabledEditing, loadingEditing, onS
                 <Bookmark01Icon />
               </button>
             )}
+            {isCreatorPath && (
+              <button
+                onClick={onSaveCreating}
+                className='text-gray-800 p-2 bg-white rounded-xl  active:scale-95 transition-all
+                    disabled:opacity-50 disabled:scale-100 disabled:hover:bg-gray-700 disabled:bg-gray-700 disabled:text-gray-300 disabled:cursor-not-allowed
+                    '
+                disabled={disabledCreating}
+              >
+                {loadingCreating ? (
+                  <svg
+                    aria-hidden='true'
+                    className='w-6 h-6 text-gray-200 animate-spin fill-gray-800'
+                    viewBox='0 0 100 101'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                      fill='currentColor'
+                    />
+                    <path
+                      d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                      fill='currentFill'
+                    />
+                  </svg>
+                ) : (
+                  <TickIcon />
+                )}
+              </button>
+            )}
+            {!isCreatorPath && !isRecordPath && (
+              <button
+                onClick={() => navigation('/create')}
+                className='text-gray-800 p-2 bg-white rounded-xl active:scale-95 transition-all'
+              >
+                <PlusIcon />
+              </button>
+            )}
           </div>
           <img
             src={user?.picture}
@@ -95,7 +159,7 @@ export default function NewPage({ children, disabledEditing, loadingEditing, onS
           />
         </div>
         <section
-          className={`bg-white md:h-[600px] md:w-full md:max-w-4xl rounded-3xl flex flex-col gap-4 pt-6 pb-4 md:pb-2 md:pt-3 md:pl-2`}
+          className={`md:relative bg-white md:h-[553px] md:w-full md:max-w-4xl rounded-3xl flex flex-col gap-4 pt-6 pb-4 md:pb-2 md:pt-3 md:pl-2`}
         >
           {children}
         </section>
@@ -114,6 +178,11 @@ export default function NewPage({ children, disabledEditing, loadingEditing, onS
         confirmLabel='Log out'
         cancelLabel='Cancel'
         onConfirm={handleLogout}
+      />
+      <GeneratorModal
+        isVisible={isGeneratorVisible}
+        onClose={() => setIsGeneratorVisible(false)}
+        onConfirm={() => setIsGeneratorVisible(false)}
       />
     </main>
   )
