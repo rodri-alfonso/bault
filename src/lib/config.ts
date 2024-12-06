b4bb604fc3366ca32ad343921d736ffb63a900c8
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-12-06 10:51:38 -0300

    [FEATURE]: Add configuration constants for password generation

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+export const UPPERCASE_LIST = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+export const LOWERCASE_LIST = UPPERCASE_LIST.toLowerCase()
+export const NUMBERS_LIST = '0123456789'
+export const SYMBOLS_LIST = '!¡@#$%&¿?[]{}<>+=-*|_.,;:^'
+
+export const MAX_PASSWORD_LENGTH = 30
+export const MIN_PASSWORD_LENGTH = 6
